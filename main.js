d74049fe8a79840d965df1257bed46777bff8fb2
Oleksandr Lopata <djelvz@gmail.com>
2022-02-21 20:05:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([50.446376, 30.538273], 10);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWx2eiIsImEiOiJja3p4MGNqYzYwMXd4Mm9vMDh5YWd3NDBtIn0.ZIKMzXZgGTIMJMiNMOpoHQ', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
